4c25b50d94f2339037c3976362bfe1f0b66c1ee6
Hesky69 <hassanismail03@gmail.com>
2024-11-25 11:17:05 +0500

    First Commit

---
+++ src/module-info.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Project {
+    requires javafx.controls;
+	requires javafx.media;
+	requires opencv;
+	requires javafx.swing;
+	requires java.desktop;
+	requires java.sql;
+    exports Project;
+}
